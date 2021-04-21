9591d2e0ea66bdb9a27aad2faf71eafcb660144e
David Bourke <davidrbourke@gmail.com>
2021-04-21 21:35:30 +0100

    Renamed class to QueryFormatter, added reusable method to remove spaces after newline

---
+++ SQLFormatter/QueryFormatter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLFormatter
+{
+    public class QueryFormatter
+    {
+        private static HashSet<string> _keywords = new HashSet<string>
+        {
+            "select",
+            "from",
+            "where"
+        };
+
+        public static string Format(string unformattedSQL)
+        {
+            var posA = 0;
+            var posB = 1;
+
+            // todo: remove all new lines 1st
+
+
+            var fsb = new StringBuilder();
+            var subStrLen = posB - posA;
+            while (posA + subStrLen <= unformattedSQL.Length)
+            {
+
+                var subStr = unformattedSQL.Substring(posA, subStrLen);
+                
+                // upper case keywords
+                foreach(var keyword in _keywords)
+                {
+                    var iOfKeyword = subStr.IndexOf(keyword);
+                    if (iOfKeyword != -1)
+                    {
+                        if (iOfKeyword > 0)
+                            fsb.AppendLine(subStr.Substring(0, iOfKeyword-1));
+
+                        fsb.AppendLine(subStr.Substring(iOfKeyword, keyword.Length).ToUpper());
+                        var nextPosB = GetNextStartPositionAfterNewline(unformattedSQL, posB + 1);
+
+                        posA = nextPosB;
+                        posB = posA + 1;
+                        break;
+                    }
+                }
+
+                // Handle comma
+                if (subStr[^1] == ',')
+                {
+                    fsb.AppendLine(subStr.Substring(0, subStrLen - 1));
+                    fsb.Append(",");
+
+                    // Need to remove whitespace after comma
+                    var nextPosB = GetNextStartPositionAfterNewline(unformattedSQL, posB + 1);
+                    
+                    posA = nextPosB;
+                    posB = posA+1;
+                }
+                                
+                posB++;
+
+                subStrLen = posB - posA;
+
+                // Check if at end
+                if (posA + subStrLen == unformattedSQL.Length)
+                {
+                    fsb.Append(unformattedSQL.Substring(posA, subStrLen));
+                }
+            }
+
+            var formattedSQL = fsb.ToString();
+            return formattedSQL;
+        }
+
+        private static int GetNextStartPositionAfterNewline(string unformattedSQL, int nextPosB)
+        {
+            while (nextPosB < unformattedSQL.Length - 1 && unformattedSQL[nextPosB] == ' ')
+            {
+                nextPosB++;
+            }
+            return nextPosB;
+        }
+
+    }
+}
--- SQLFormatterTests/FormatterTests.cs
+++ SQLFormatterTests/FormatterTests.cs
@@ -6,15 +6,19 @@ namespace SQLFormatterTests
 {
     public class FormatterTests
     {
-        [Fact]
-        public void FormatSQL_Simple_UppercaseKeywords()
-        {
-            var result = Formatter.FormatSQL("select * from dbo.table ");
 
-            result.Should().Be(@"SELECT
-*
+        [Fact]
+        public void Format_Commas_OnNewLine()
+        {
+            var formatted = QueryFormatter.Format("select cola, colb, colc from dbo.table where cola = \"test\"");
+            Assert.Equal(@"SELECT
+cola
+,colb
+,colc
 FROM
-dbo.table ");
+dbo.table
+WHERE
+cola = ""test""", formatted);
         }
     }
 }
